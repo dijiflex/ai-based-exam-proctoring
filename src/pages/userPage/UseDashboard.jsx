136c9ff0b04b2cc7eaa9d5e4b6d88caf258c5284
dijiflex <dijiflex@gmail.com>
2021-07-30 10:57:33 +0300

    Implemented start and end exmas

--- src/pages/userPage/UseDashboard.jsx
+++ src/pages/userPage/UseDashboard.jsx
@@ -1,31 +1,42 @@
 /* eslint-disable no-unused-expressions */
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Container, Grid, Paper } from '@material-ui/core';
-import PersonGroup from '../../Components/PersonGroup/PersonGroup';
+import { Container, Grid, makeStyles, Paper, Button } from '@material-ui/core';
 import Pdf from '../../Components/pdfViewer/Pdf';
 
 import MainWebcam from '../../Components/Webcam/MainWebcam';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(1),
+    padding: theme.spacing(1)
+  }
+}));
+
 const UseDashboard = () => {
+  const classes = useStyles();
+  const [exam, setExam] = useState(false);
   return (
     <Container>
       <Grid container spacing={1}>
         <Grid container spacing={1}>
           <Grid item xs={12} sm={6}>
-            <Paper elevation={1}>
+            <Paper className={classes.root} elevation={1}>
               <MainWebcam />
             </Paper>
           </Grid>
           <Grid item xs={12} sm={6}>
-            <Paper style={{ height: '100%' }} elevation={1}>
-              this is a page
+            <Paper style={{ height: '89%' }} className={classes.root} elevation={1}>
+              <Button variant="contained" color="primary" onClick={() => setExam(!exam)}>
+                {exam ? 'End Exam' : 'Start Exam'}
+              </Button>
             </Paper>
           </Grid>
         </Grid>
         <Grid container spacing={1} justifyContent="center">
           <Grid item xs={12} sm={7}>
-            <Pdf />
+            {exam ? <Pdf /> : null}
           </Grid>
         </Grid>
       </Grid>
